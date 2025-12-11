3330411de25ee2b22300374bc8d989e9986f6363
hyeladee <dave4digits@gmail.com>
2025-12-11 11:31:54 +0000

    fix: removed trailing slash form view_profile irl

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'user_profile'
 
 urlpatterns = [
-  path("/", views.view_profile, name="view_profile"),
+  path("", views.view_profile, name="view_profile"),
   path("edit/", views.edit_profile, name="edit_profile"),
   # No need for new profile URL, as django signal can quickly setup a new profile upon user creation
   # path("new/", views.new_profile, name="new_profile"),
