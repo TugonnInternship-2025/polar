863a4f01b7eb0cf73bf26c152471e8f73c1f4fbd
hyeladee <dave4digits@gmail.com>
2025-12-11 11:16:42 +0000

    update: defined user model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  image = CloudinaryField('image')
+  bio = models.TextField(null=True, blank=True) # Optional
+  location = models.CharField(max_length=50)
+  phone_number = models.CharField(max_length=20)
+  portfolio_url = models.CharField(max_length=100, null=True, blank=True) # Optional
