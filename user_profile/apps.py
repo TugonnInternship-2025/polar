b20f019c2760abaee7ab484a08c23d2f3088fda8
hyeladee <dave4digits@gmail.com>
2025-12-13 20:49:51 +0000

    update: added signal to auto create a user profile whenever a new user is created

--- user_profile/apps.py
+++ user_profile/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class UserProfileConfig(AppConfig):
     name = 'user_profile'
+    
+    # This function loads the signal that creates a user_profile upon a new user creation
+    def ready(self):
+        import user_profile.signals
---
+++ user_profile/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import UserProfile
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+      """
+      Creates a UserProfile automatically when a new User is created.
+      """
+  
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+      """
+      Updates the UserProfile whenever the User is saved.
+      
+      This ensures that if the User object is saved, the linked Profile is also 
+      saved, keeping them in sync.
+      """
+      
+      instance.userprofile.save()
