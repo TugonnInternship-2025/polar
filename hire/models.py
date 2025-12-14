c42275baf7249ef8f2cd22741bdfde3e6803a0d1
onetyten <labaekaabdulrazaq@gmail.com>
2025-12-14 16:29:21 +0100

    Hire model

--- hire/models.py
+++ hire/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class HireModel(models.Model):
+    STATUS_PENDING = 'pending'
+    STATUS_COMPLETED = 'completed'
+    STATUS_CANCELLED = 'cancelled'
+    
+    STATUS_CHOICES = [
+        (STATUS_PENDING, 'Pending'),
+        (STATUS_COMPLETED, 'Completed'),
+        (STATUS_CANCELLED, 'Cancelled'),
+    ]
+    
+    client = models.ForeignKey(User,on_delete=models.CASCADE, related_name='hires_made')
+    talent = models.ForeignKey(User,on_delete=models.CASCADE, related_name='hires_gotten')
+    talent_name = models.CharField(max_length=255)
+    talent_location = models.CharField(max_length=255)
+    talent_contact = models.CharField(max_length=255)
+    talent_email = models.EmailField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_PENDING)
+    hired_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True,blank=True)
+    
+    def __str__(self):
+        return f"Hire {self.id} - {self.talent_name} - ({self.status})"
+    
